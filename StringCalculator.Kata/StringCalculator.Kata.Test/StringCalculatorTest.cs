cd3d6def32a9f7f983fbf27de1c4e8b040cbbbcd
Camilo Garaventa <camilogaraventa@gmail.com>
2017-08-15 11:22:04 -0300

    Initial Commit

---
+++ StringCalculator.Kata/StringCalculator.Kata.Test/StringCalculatorTest.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Kata.Test
+{
+    /// <summary>
+    /// Descripción resumida de StringCalculatorTest
+    /// </summary>
+    [TestClass]
+    public class StringCalculatorTest
+    {
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Obtiene o establece el contexto de las pruebas que proporciona
+        ///información y funcionalidad para la serie de pruebas actual.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void ReturnsZero_WhenStringEmpty()
+        {
+            Int32 result = StringCalculator.Add(String.Empty);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void ReturnsTheSameNumber_WhenUsingOneNumber()
+        {
+            String numbers = "5";
+
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void ReturnsSumOfTwoNumbers_WhenUsingTwoNumbersSeparatedByComma()
+        {
+            String numbers = "5,2";
+
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void ReturnsSumOfNumbers_WhenUsingFourNumbersSeparatedByComma()
+        {
+            String numbers = "5,2,6,8";
+
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(21, result);
+        }
+
+        [TestMethod]
+        public void ReturnsSumOfNumbers_WhenUsingCommasAndNewLineSeparator()
+        {
+            String numbers = "5,2\n6,8";
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(21, result);
+        }
+
+        [TestMethod]
+        public void ReturnsSumOfNumbers_WhenUsingCustomDelimiters()
+        {
+            String numbers = "//:\n1:2:3";
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void ThrowsException_WhenUsingNegativeNumber()
+        {
+            String numbers = "-2";
+            String errorMessage = String.Empty;
+
+            try
+            {
+                StringCalculator.Add(numbers);
+            }
+            catch (Exception e )
+            {
+                errorMessage = e.Message;                
+            }
+
+            Assert.IsTrue(errorMessage.Contains("-2"));
+
+        }
+
+        [TestMethod]
+        public void ThrowsException_WhenUsingMultipleNegativeNumbers()
+        {
+            String numbers = "-2,-4,3,-6,2";
+            String errorMessage = String.Empty;
+
+            try
+            {
+                StringCalculator.Add(numbers);
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+            }
+
+            Assert.IsTrue(errorMessage.Contains("-2, -4, -6"));
+
+        }
+
+        [TestMethod]
+        public void ExcludeNumbers_WhenUsingNumbersGreaterThanThousand()
+        {
+            String numbers = "2,1001";
+
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void ReturnSumNumbers_WhenUsingThounsandValue()
+        {
+            String numbers = "2,1000";
+
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(1002, result);
+        }
+
+        [TestMethod]
+        public void ReturnSumNumbers_WhenUsingDelimitersOfAnyLenght()
+        {
+            String numbers = "//[***]\n1***2***3";
+
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void ReturnSumNumbers_WhenUsingMultipleCustomDelimiters()
+        {
+            String numbers = "//[*][&%]\n1*2&%3";
+
+            Int32 result = StringCalculator.Add(numbers);
+
+            Assert.AreEqual(6, result);
+        }
+    }
+}
---
+++ StringCalculator.Kata/StringCalculator.Kata/StringCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Kata
+{
+    public class StringCalculator
+    {
+        public static Int32 Add(String numbers)
+        {
+            if (!String.IsNullOrWhiteSpace(numbers))
+            {
+                String[] delimiters = GetDelimiters(numbers);
+
+                numbers = RemoveDelimiterLine(numbers);
+
+                String[] numbersList = numbers.Split(delimiters, StringSplitOptions.None);
+
+                ValidateNegativeNumbers(numbersList);
+
+                return numbersList.Where(n => Int32.Parse(n) <= 1000).Sum(n => Int32.Parse(n));
+            }
+
+            return 0;
+        }
+
+        private static void ValidateNegativeNumbers(String[] numbersList)
+        {
+            List<String> negativesNumbers = numbersList.Where(x => Int32.Parse(x) < 0).ToList();
+
+            if(negativesNumbers.Any())
+            {
+                throw new Exception(String.Format("Negatives not allowed: {0}",String.Join(", ", negativesNumbers.ToArray())));
+            }
+        }
+
+        private static String RemoveDelimiterLine(String numbers)
+        {
+            if (numbers.Length > 1 && numbers.Substring(0, 2) == "//")
+            {
+                return numbers.Substring(numbers.IndexOf("\n") + 1);
+            }
+            else
+            {
+                return numbers;
+            }
+        }
+
+        private static String[] GetDelimiters(String numbers)
+        {
+            String[] delimiters;
+            if (numbers.Length > 1 && numbers.Substring(0, 2) == "//")
+            {
+                String delimiterLine = numbers.Substring(2,numbers.IndexOf("\n")-2);
+                delimiters = delimiterLine.Split('[');
+                for (Int32 i = 0; i < delimiters.Length;i++ )
+                {
+                    delimiters[i] = delimiters[i].Replace("]", String.Empty);
+                }
+            }
+            else
+            {
+                delimiters = new String[2];
+                delimiters[0] = "\n";
+                delimiters[1] = ",";                
+            }
+
+            return delimiters;
+        }
+    }
+}
